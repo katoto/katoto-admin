776241b4a57913d5b64ecdf2c97e1b705915d27e
coinslot <xiezg@qq.com>
2019-04-22 17:32:48 +0800

    422

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   BASE_API: '"http://prewww.madcrickets.com/onlineadmin"'
-//   BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+// http://prewww.madcrickets.com/preadmin/
 //   BASE_API: '"https://www.easy-mock.com/mock/5c88aeab4c25252b7c484902/katoto"',
 }
